28fa78eb5bd3b79c77b7ccd39d2cad707c700101
shaked cohen <shakedcohen@shakeds-MacBook-Air.local>
2025-11-27 20:10:18 +0200

    finish hw 4 hopefully

--- MyString.java
+++ MyString.java
@@ -18,13 +18,58 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String newS = "";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i); // take a letter in index i
+            if (ch >= 'A' && ch <= 'Z') { // if letter is big
+                newS += (char) (ch + 32); // make lettr smaller +32 and change back to char then add to the new string
+            } else {
+                newS = newS + ch; // if letter is already small just add her to the new string
+            }
+        }
+        return newS;
+
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
-        return false;
+
+    for (int i = 0; i <= str1.length() - str2.length(); i++) {
+        int count = 0; 
+        for (int j = 0; j < str2.length(); j++) {
+            if (str1.charAt(i + j) == str2.charAt(j)) 
+                count++;
+            else 
+                break;                  
+        }
+
+        if (count == str2.length()) {
+            return true; 
+        }
     }
+
+    return false;
+}
+
+    /*public static boolean contains(String str1, String str2) {
+        if (str2.length() == 0)
+            return true;
+
+        for (int i = 0; i <= str1.length() - str2.length(); i++) { // run on str2
+            boolean match = true;
+            for (int j = 0; j < str2.length(); j++) {
+                if (str1.charAt(i + j) != str2.charAt(j)) {// run on str1
+                    match = false;
+                    break;
+                }
+            }
+            if (match)
+                return true;
+        }
+        return false;
+    }*/
+
+
+
+
 }
--- Primes.java
+++ Primes.java
@@ -1,5 +1,44 @@
 public class Primes {
+
     public static void main(String[] args) {
-        // Replace this statement with your code
+
+        int n = Integer.parseInt(args[0]);
+
+        // create boolean array
+        boolean[] isPrime = new boolean[n + 1];
+
+        // assume all numbers bigger then 2 are prime
+        for (int i = 2; i <= n; i++) {
+            isPrime[i] = true;
+        }
+
+        // algorithm
+        int p = 2;
+        while (p * p <= n) { // all the multiples of p -> gone
+            if (isPrime[p]) {
+                // eliminate multiples of p
+                int multiple = p * p; 
+                while (multiple <= n) { 
+                    isPrime[multiple] = false;
+                    multiple += p;
+                }
+            }
+            p++;
+        }
+
+        // print primes 
+        System.out.println("Prime numbers up to " + n + ":");
+        int count = 0;
+        for (int i = 2; i <= n; i++) {
+            if (isPrime[i]) { //if it is prime - 'true' so print it
+                System.out.println(i);
+                count++; // count how many primes we have
+            }
+        }
+
+        //  print a message with percentage 
+        int percent = (int) (100.0 * count / n);// calaulate percentage
+        System.out.println("There are " + count + " primes between 2 and " + n +
+                " (" + percent + "% are primes)");
     }
 }
